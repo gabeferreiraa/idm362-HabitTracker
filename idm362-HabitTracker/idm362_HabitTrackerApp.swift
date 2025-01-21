03ae8355366b1ef7a59256a74eb910b0c86fa550
gabeferreiraa <146259686+gabeferreiraa@users.noreply.github.com>
2025-01-21 18:49:47 -0500

    Initial Commit

---
+++ idm362-HabitTracker/idm362_HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  idm362_HabitTrackerApp.swift
+//  idm362-HabitTracker
+//
+//  Created by Gabriel Ferreira on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct idm362_HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
