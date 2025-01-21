2f72f8e31f51890f2cb44a9684d3fde0813f05cd
gabeferreiraa <146259686+gabeferreiraa@users.noreply.github.com>
2025-01-21 18:52:54 -0500

    first commit new repo

--- idm362-HabitTracker/ContentView.swift
+++ idm362-HabitTracker/ContentView.swift
@@ -1,8 +1,8 @@
 //
 //  ContentView.swift
-//  idm362-HabitTracker
+//  idm362-Gabe
 //
-//  Created by Gabriel Ferreira on 1/21/25.
+//  Created by Gabriel Ferreira on 1/14/25.
 //
 
 import SwiftUI
@@ -10,12 +10,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("gabe_headshot5-copy")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+                .cornerRadius(20)
+                .padding()
+            
+            Text("My name's Gabe!")
+                .font(.title)
+                .padding()
+            
+            Button(action: {
+                print("Contacted!")
+            }) {
+                Text("Contact Me")
+                    .font(.headline)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .padding(30)
         }
-        .padding()
     }
 }
 
