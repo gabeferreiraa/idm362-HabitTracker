746758793cf729f06939b4b9f3555e42f3e3785c
gabeferreiraa <146259686+gabeferreiraa@users.noreply.github.com>
2025-01-28 20:39:24 -0500

    Initial Commit

---
+++ idm362-HabitTracker-test1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  idm362-HabitTracker-test1
+//
+//  Created by Gabriel Ferreira on 1/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ idm362-HabitTracker-test1/idm362_HabitTracker_test1App.swift
@@ -0,0 +1,17 @@
+//
+//  idm362_HabitTracker_test1App.swift
+//  idm362-HabitTracker-test1
+//
+//  Created by Gabriel Ferreira on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct idm362_HabitTracker_test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
